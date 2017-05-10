2ba35c32872ec9ab6f852a854a5f771b6e3cbc46
Peng Zhao <baydap@gmail.com>
2017-05-10 14:21:22 +0200

    Version 0.1
    a very preliminary version.

--- _runme.R
+++ _runme.R
@@ -1,21 +1,25 @@
-title <- 'bookdown+: Authoring Articles, Mails, Guitar Chords, Chemical Molecular Formulae and Equations with R bookdown'
+## ------------------------------------------------------------------------
+title <- 'bookdown+: Authoring Articles, Mails, Guitar books, Chemical Molecular Formulae and Equations with R bookdown'
 author <- 'Peng Zhao'
 
-templates  <- c('article',     #1
-                'mini',        #2
-                'original',    #3
-                'book_zh',     #4
-                'article_zh',  #5
-                'calendar',    #6
-                'chemistry',   #7
-                'chemistry_zh',#8
-                'guitar',      #9
-                'mail',        #10
-                'journal'      #11
+## ------------------------------------------------------------------------
+templates  <- c('article',     #1, academic article
+                'article_zh',  #2, academic article in Chinese
+                'calendar',    #3, calendar, in progress
+                'chemistry',   #4, chemical molecular formulae and equations
+                'chemistry_zh',#5, in Chinese,
+                'guitar',      #6, guitar book
+                'mail',        #7, mail, in progress
+                'journal',     #8, laboratory journal, or personal diary
+                'mini',        #9, a mini demo by Yihui
+                'original',    #10, the original demo by Yihui
+                'book_zh'      #11, the demo in Chinese by Yihui
 )
+template <- templates[8]
 
-template <- templates[11]
-
+## ------------------------------------------------------------------------
 source('fun/build.R')
 
-# Now you can press ctrl+shift+b to get your lovely book. Have fun!
+## ------------------------------------------------------------------------
+bookdown::render_book('index.Rmd', 'all')
+
